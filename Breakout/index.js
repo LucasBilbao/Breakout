e7a9b1fd3122284d77e63862ae27fda1784732d8
LucasBilbao <lucasbm789@gmail.com>
2021-11-05 21:09:25 +0400

    fix: remove redundancies

--- Breakout/index.js
+++ Breakout/index.js
@@ -269,13 +269,9 @@ function removeAt(arr, index) {
   return res;
 }
 
-let h1 = document.createElement('h1');
-document.body.appendChild(h1);
-
 function getMousePos(canvas, evt) {
   const rect = canvas.getBoundingClientRect();
 
-  h1.innerHTML = `${isTouchDevice()}`;
   if (isTouchDevice()) {
     return {
       x: evt.touches[0].clientX - rect.left,
